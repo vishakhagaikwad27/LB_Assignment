23f14d0376d4094dba091cb048b6a2e5049b66eb
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-03-22 22:49:25 +0530

    Add files via upload

---
+++ 04 Pattern Printing/01 Full Star.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= 4; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    printf("\n========THANKS=========");
+    getch();
+    return 0;
+}
+
---
+++ 04 Pattern Printing/02 Alternative Printing.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= 3; i++)
+    {
+        for(j = 1; j <= 4; j++)
+        {
+            if(j % 2 == 0)
+            {
+                printf(" # ");
+            }
+            else
+            {
+                printf(" * ");
+            }
+        }
+        printf("\n");
+    }
+}
