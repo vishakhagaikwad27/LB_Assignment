9c483c89bdf98b5e97efd3d8563202bdd369a9a9
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-06-09 22:25:59 +0530

    Add files via upload

---
+++ 07 Assignment On Array/01 Create Array Without initialization and Display All Elements.c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[5]={};
+    printf("\n This is 1st Element=%d",Num[0]);
+    printf("\n This is 2nd Element=%d",Num[1]);
+    printf("\n This is 3rd Element=%d",Num[2]);
+    printf("\n This is 4th Element=%d",Num[3]);
+    printf("\n This is 5th Element=%d",Num[4]);
+    printf("\n=============THANKS==============");
+    getch();
+    return 0;
+}
