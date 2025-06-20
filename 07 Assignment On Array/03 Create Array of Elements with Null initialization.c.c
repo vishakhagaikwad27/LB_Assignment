952c85cca6b8ac8140f57f43fe8b151602f1e7f8
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-06-20 22:01:23 +0530

    Array of Elements with Null Initialization

---
+++ 07 Assignment On Array/03 Create Array of Elements with Null initialization.c.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+          int Num[7]={};
+          printf("\n Enter a 1st element=");
+          scanf("%d",&Num[0]);
+          printf("\n Enter a 2nd element=");
+          scanf("%d",&Num[1]);
+          printf("\n Enter a 3rd element=");
+          scanf("%d",&Num[2]);
+          printf("\n Enter a 4th element=");
+          scanf("%d",&Num[3]);
+          printf("\n Enter a 5th element=");
+          scanf("%d",&Num[4]);
+          printf("\n Enter a 6th element=");
+          scanf("%d",&Num[5]);
+          printf("\n Enter a 7th element=");
+          scanf("%d",&Num[6]);
+
+          printf("\n Value of 1st Element is %d",Num[0]);
+          printf("\n Value of 2nd Element is %d",Num[1]);
+          printf("\n Value of 3rd Element is %d",Num[2]);
+          printf("\n Value of 4th Element is %d",Num[3]);
+          printf("\n Value of 5th Element is %d",Num[4]);
+          printf("\n Value of 6th Element is %d",Num[5]);
+          printf("\n Value of 7th Element is %d",Num[6]);
+          printf("\n==========THANKS============");
+          getch();
+          return 0;
+}
---
+++ 07 Assignment On Array/04 Create Array of Elements with Null initialization Then Accept and Display Without Loop.c.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[7]={};
+    printf("\n Enter a 1st Value=");
+    scanf("%d",& Num[0]);
+    printf("\n Enter a 2nd Value=");
+    scanf("%d",& Num[1]);
+    printf("\n Enter a 3rd Value=");
+    scanf("%d",& Num[2]);
+    printf("\n Enter a 4th Value=");
+    scanf("%d",& Num[3]);
+    printf("\n Enter a 5th Value=");
+    scanf("%d",& Num[4]);
+    printf("\n Enter a 6th Value=");
+    scanf("%d",& Num[5]);
+    printf("\n Enter a 7th Value=");
+    scanf("%d",& Num[6]);
+
+    printf("\n This is Value of 1st Element=%d",Num[0]);
+    printf("\n This is Value of 2nd Element=%d",Num[1]);
+    printf("\n This is Value of 3rd Element=%d",Num[2]);
+    printf("\n This is Value of 4th Element=%d",Num[3]);
+    printf("\n This is Value of 5th Element=%d",Num[4]);
+    printf("\n This is Value of 6th Element=%d",Num[5]);
+    printf("\n This is Value of 7th Element=%d",Num[6]);
+    printf("\n=================THANKS==================");
+    getch();
+    return 0;
+}
